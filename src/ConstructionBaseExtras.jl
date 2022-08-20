c2145b18830dd207ba5674afeb3db9aadd2f5fd0
Alexander Plavin <alexander@plav.in>
2022-08-20 07:00:12 +0300

    StaticArraysCore instead of Requires

--- src/ConstructionBaseExtras.jl
+++ src/ConstructionBaseExtras.jl
@@ -1,11 +1,7 @@
 module ConstructionBaseExtras
 
-using ConstructionBase, Requires
+using ConstructionBase
 
-function __init__()
-    @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" begin
-        include("staticarrays.jl")
-    end
-end
+include("staticarrays.jl")
 
 end
