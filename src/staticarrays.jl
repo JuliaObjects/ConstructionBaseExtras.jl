8de370cce4e6aac7f32d3b30c92d6f33ff6ecfb8
Alexander Plavin <alexander@plav.in>
2022-08-25 16:11:08 +0300

    simplify even further

--- src/staticarrays.jl
+++ src/staticarrays.jl
@@ -1,18 +1,8 @@
 using StaticArraysCore
 
-struct SArrayConstructor{S} end
-struct MArrayConstructor{S} end
-struct SizedArrayConstructor{S,N,M} end
-
-(::SArrayConstructor{S})(args...) where {S} = SArray{S}(args...)
-(::MArrayConstructor{S})(args...) where {S} = MArray{S}(args...)
-(::SizedArrayConstructor{S,N,M})(data::AbstractArray{T,M}) where {S,T,N,M} = 
-    SizedArray{S,T,N,M}(data)
-
-ConstructionBase.constructorof(sa::Type{<:SArray{S}}) where {S} = SArrayConstructor{S}()
-ConstructionBase.constructorof(sa::Type{<:MArray{S}}) where {S} = MArrayConstructor{S}()
-ConstructionBase.constructorof(sa::Type{<:SizedArray{S,<:Any,N,M}}) where {S,N,M} = 
-    SizedArrayConstructor{S,N,M}()
+ConstructionBase.constructorof(sa::Type{<:SArray{S}}) where {S} = SArray{S}
+ConstructionBase.constructorof(sa::Type{<:MArray{S}}) where {S} = MArray{S}
+ConstructionBase.constructorof(sa::Type{<:SizedArray{S}}) where {S} = SizedArray{S}
 
 # static vectors don't even need the explicit size specification
 ConstructionBase.constructorof(::Type{SVector}) = SVector
