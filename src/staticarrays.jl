04a06d37ce1c2a9374991b8497237f7b034a0f18
Alexander Plavin <alexander@plav.in>
2022-08-25 16:08:37 +0300

    simplify and generalize

--- src/staticarrays.jl
+++ src/staticarrays.jl
@@ -1,20 +1,19 @@
 using .StaticArrays
 
-struct SArrayConstructor{S,N,L} end
-struct MArrayConstructor{S,N,L} end
+struct SArrayConstructor{S} end
+struct MArrayConstructor{S} end
 struct SizedArrayConstructor{S,N,M} end
 
-(::SArrayConstructor{S,N,L})(data::NTuple{L,T}) where {S,T,N,L} = SArray{S,T,N,L}(data)
-(::MArrayConstructor{S,N,L})(data::NTuple{L,T}) where {S,T,N,L} = MArray{S,T,N,L}(data)
+(::SArrayConstructor{S})(args...) where {S} = SArray{S}(args...)
+(::MArrayConstructor{S})(args...) where {S} = MArray{S}(args...)
 (::SizedArrayConstructor{S,N,M})(data::AbstractArray{T,M}) where {S,T,N,M} = 
     SizedArray{S,T,N,M}(data)
 
-ConstructionBase.constructorof(sa::Type{<:SArray{S,<:Any,N,L}}) where {S,N,L} = 
-    SArrayConstructor{S,N,L}()
-ConstructionBase.constructorof(sa::Type{<:MArray{S,<:Any,N,L}}) where {S,N,L} = 
-    MArrayConstructor{S,N,L}()
+ConstructionBase.constructorof(sa::Type{<:SArray{S}}) where {S} = SArrayConstructor{S}()
+ConstructionBase.constructorof(sa::Type{<:MArray{S}}) where {S} = MArrayConstructor{S}()
 ConstructionBase.constructorof(sa::Type{<:SizedArray{S,<:Any,N,M}}) where {S,N,M} = 
     SizedArrayConstructor{S,N,M}()
 
+# static vectors don't even need the explicit size specification
 ConstructionBase.constructorof(::Type{SVector}) = SVector
 ConstructionBase.constructorof(::Type{MVector}) = MVector
