35c8e28bd08837f4ec08e87f792c07e6227707a1
Alexander Plavin <alexander@plav.in>
2022-08-25 20:26:27 +0300

    even better vector handling

--- src/staticarrays.jl
+++ src/staticarrays.jl
@@ -5,5 +5,5 @@ ConstructionBase.constructorof(sa::Type{<:MArray{S}}) where {S} = MArray{S}
 ConstructionBase.constructorof(sa::Type{<:SizedArray{S}}) where {S} = SizedArray{S}
 
 # static vectors don't even need the explicit size specification
-ConstructionBase.constructorof(::Type{SVector}) = SVector
-ConstructionBase.constructorof(::Type{MVector}) = MVector
+ConstructionBase.constructorof(::Type{<:SVector}) = SVector
+ConstructionBase.constructorof(::Type{<:MVector}) = MVector
